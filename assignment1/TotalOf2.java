04a4062dd7eebe2813108b92c22397b493654d9d
TIRTH <tirthp0703@gmail.com>
2026-07-23 14:17:31 +0530

    made corrections

--- assignment1/TotalOf2.java
+++ assignment1/TotalOf2.java
@@ -3,10 +3,10 @@ public class TotalOf2{
     public static void main(String args[]){
         System.out.println("Enter marks of two subjects: ");
         Scanner sc = new Scanner(System.in);
-        Marks M1 = new Marks(sc.nextInt(), sc.nextInt());
+        //Marks M1 = new Marks(sc.nextInt(), sc.nextInt());
+        //System.out.println("Total Marks : " + M1.totalMarks());
+        Marks M1 = new Marks(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
         System.out.println("Total Marks : " + M1.totalMarks());
-        // Marks M1 = new Marks(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-        // System.out.println("Total Marks : " + M1.totalMarks());
         sc.close();
     }
 }
--- assignment2/Vector.java
+++ assignment2/Vector.java
@@ -3,58 +3,82 @@ public class Vector {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter values of first vector : ");
-        VectorOperations V1 = new VectorOperations(sc.nextDouble(), sc.nextDouble(), sc.nextDouble());
+        VOperations V1 = new VOperations(sc.nextDouble(), sc.nextDouble(), sc.nextDouble());
         System.out.println("Enter values of second vector : ");
-        VectorOperations V2 = new VectorOperations(sc.nextDouble(), sc.nextDouble(), sc.nextDouble());
-        VectorOperations resultAdd = V2.add(V1);
-        System.out.println("Addition = " + resultAdd.x + "i +  " + resultAdd.y + "j + " + resultAdd.z + "k");
-        VectorOperations resultSub = V2.subtract(V1);
-        System.out.println("Subtraction = " + resultSub.x + "i +  " + resultSub.y + "j + " + resultSub.z + "k");
-        VectorOperations resultVProduct = V2.vectorProduct(V1);
-        System.out.println("Vector Product = " + resultVProduct.x + "i +  " + resultVProduct.y + "j + " + resultVProduct.z + "k");
+        VOperations V2 = new VOperations(sc.nextDouble(), sc.nextDouble(), sc.nextDouble());
+        System.out.println("Enter scalar number : ");
+        double num = sc.nextDouble();
+        VOperations resultAdd = V2.add(V1);
+        VOperations resultSub = V2.subtract(V1);
+        VOperations resultVProduct = V2.vectorProduct(V1);
+        VOperations resultSMultiply = V1.scalarMultiply(num);
+        VOperations resultSDivide = V1.scalarDivide(num);
         double resultDProduct= V2.dotProduct(V1);
-        System.out.println("Dot Product = " + resultDProduct);
+        System.out.print("Addition = ");
+        resultAdd.display();
+        System.out.print("Subtraction = ");
+        resultSub.display();
+        System.out.print("Vector Product = ");
+        resultVProduct.display();
+        System.out.print("Multiplication with Scalar = ");
+        resultSMultiply.display();
+        System.out.print("Division with Scalar = ");
+        resultSDivide.display();
+        System.out.println("Dot Product = "  + resultDProduct);
         sc.close();
     }
 }
-class VectorOperations{
+class VOperations{
     double x;
     double y;
     double z;
 
-    VectorOperations(double x, double y, double z){
+    VOperations(double x, double y, double z){
         this.x = x;
         this.y = y;
         this.z = z;
     }
     
-    VectorOperations add(VectorOperations other){
+    void display(){
+        System.out.println(this.x + "i + " + this.y + "j + " + this.z + "k");
+    }
+
+    VOperations add(VOperations other){
         double x = this.x + other.x;
         double y = this.y + other.y;
         double z = this.z + other.z;
-        return new VectorOperations(x, y, z);
+        return new VOperations(x, y, z);
     }
 
-    VectorOperations subtract(VectorOperations other){
+    VOperations subtract(VOperations other){
         double x = this.x - other.x;
         double y = this.y - other.y;
         double z = this.z - other.z;
-        return new VectorOperations(x, y, z);
+        return new VOperations(x, y, z);
     }
 
-    VectorOperations vectorProduct(VectorOperations other){
+    VOperations vectorProduct(VOperations other){
         double x = (this.z * other.y) - (this.y * other.z);
         double y = (this.x * other.z) - (this.z * other.x);
         double z = (this.y * other.x) - (this.x * other.y);
-        return new VectorOperations(x, y, z);
+        return new VOperations(x, y, z);
     }
 
-    double dotProduct(VectorOperations other){
+    VOperations scalarMultiply(double scalar){
+        double x = this.x * scalar;
+        double y = this.y * scalar;
+        double z = this.z * scalar;
+        return new VOperations(x, y, z);
+    }
+
+    VOperations scalarDivide(double scalar){
+        double x = this.x / scalar;
+        double y = this.y / scalar;
+        double z = this.z / scalar;
+        return new VOperations(x, y, z);
+    }
+
+    double dotProduct(VOperations other){
         return (this.x * other.x) + (this.y * other.y) + (this.z * other.z);
     }
-
-    // VectorOperations scalarMultiply(double scalar){
-
-    // }
-
 }
